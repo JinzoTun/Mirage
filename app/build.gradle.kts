ba98473b9f0bdceb8a39367e226281f968c769c2
Koussay Hajri <hajrikoussay03@gmail.com>
2023-11-29 12:43:06 +0100

    Update build.gradle.kts
    added firebase dependecies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,12 @@ android {
 }
 
 dependencies {
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
